462fe50dc44964206ea20c0c5ced6e5a1f14d808
Aimeos <aimeos@aimeos.org>
2015-04-19 19:34:15 +0200

    Require RealURL and update versions

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,17 +21,17 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '1.1.0',
+	'version' => '1.2.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '6.2.0-7.99.99',
-			'aimeos' => '2.1.0-2.99.99',
+			'aimeos' => '2.2.0-2.99.99',
 			'bootstrap_package' => '6.2.0-6.99.99',
+			'realurl' => '1.13.3-1.99.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
-			'realurl' => '1.12.8-1.99.99'
 		),
 	),
 );
